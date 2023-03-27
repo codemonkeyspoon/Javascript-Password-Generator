237c200c75fc22677f7307205043b48b26cc3a19
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-03-27 15:22:54 -0400

    Removed modal in favor of alert boxes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,59 +1,66 @@
-const modal = document.getElementById("modal");
-const btn = document.getElementById("open-modal");
-const result = document.querySelector('#result');
-const passLength = document.querySelector('#length');
-const passLengthResult = document.querySelector('#length-result');
-const includeLowerCase = document.querySelector('#lowercase')
-const includeUpperCase = document.querySelector('#uppercase')
-const includeNumbers = document.querySelector('#numbers');
-const includeSymbols = document.querySelector('#spec-char');
-const generateBtn = document.querySelector('#generate');
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// Set default password length to 15
-document.addEventListener('DOMContentLoaded', () => {
-    passLength.value = 15
-    passLengthResult.innerText = 15
-})
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// Listen for password range change
-passLength.addEventListener('change', (event) => {
-    passLengthResult.innerText = event.target.value
-})
+  passwordText.value = password;
 
-function generatePassword(length, useUppercase, useLowercase, useNumbers, useSymbols) {
-    var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    var numberChars = '0123456789';
-    var symbolChars = '!@#$%^&*()_+{}[];:<>,.?/~`|-=';
-  
-    var allowedChars = '';
-    if (useUppercase) allowedChars += uppercaseChars;
-    if (useLowercase) allowedChars += lowercaseChars;
-    if (useNumbers) allowedChars += numberChars;
-    if (useSymbols) allowedChars += symbolChars;
-  
-    var password = '';
-    for (var i = 0; i < length; i++) {
-      var randomIndex = Math.floor(Math.random() * allowedChars.length);
-      password += allowedChars[randomIndex];
-    }
-    return password;
 }
 
-generateBtn.addEventListener('click', () => {
-    const password = generatePassword(
-        passLength.value,
-        includeUpperCase.checked,
-        includeLowerCase.checked,
-        includeNumbers.checked,
-        includeSymbols.checked
-    );
-    result.innerText = password;
-    modal.style.display = "none";
-    btn.style.display = "block"
-});
-
-btn.onclick = function() {
-    modal.style.display = "block";
-    btn.style.display = "none"
+// function to generate password
+function generatePassword() {
+  // Define character sets
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var symbols = "!@#$%^&*()_+-=[]{}|;':\",./<>?\\";
+  
+  // Initialize password variable
+  var password = "";
+  
+  // Prompt user for password criteria
+  var length = parseInt(prompt("How long would you like your password to be? (between 8 and 128 characters)"));
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid length. Please enter a number between 8 and 128.");
+    return "";
+  }
+  
+  var useLowercase = confirm("Would you like to use lowercase characters?");
+  var useUppercase = confirm("Would you like to use uppercase characters?");
+  var useNumbers = confirm("Would you like to use numbers?");
+  var useSymbols = confirm("Would you like to use symbols?");
+  
+  // Check if at least one character set is selected
+  if (!useLowercase && !useUppercase && !useNumbers && !useSymbols) {
+    alert("You must select at least one character set.");
+    return "";
+  }
+  
+  // Generate password
+  var availableChars = "";
+  if (useLowercase) {
+    availableChars += lowercase;
+  }
+  if (useUppercase) {
+    availableChars += uppercase;
+  }
+  if (useNumbers) {
+    availableChars += numbers;
+  }
+  if (useSymbols) {
+    availableChars += symbols;
+  }
+  
+  for (var i = 0; i < length; i++) {
+    password += availableChars.charAt(Math.floor(Math.random() * availableChars.length));
+  }
+  
+  return password;
 }
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
